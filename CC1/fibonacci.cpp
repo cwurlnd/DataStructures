f17343dc7dbb0231aabbef196f541f3bd41ff60b
Corey Wurl <cwurl@student06.cse.nd.edu>
2020-08-22 18:33:00 -0400

    CC 1 Update

---
+++ CC1/fibonacci.cpp
@@ -0,0 +1,30 @@
+/***************
+ * File: fibonacci.cpp
+ * Author: Corey Wurl
+ * NDID: 902091236
+ *
+ * This file contains the function declarations
+ * for my Fibonacci application
+ **************/
+
+#include "fibonacci.h"
+
+void fibonacci(int numRows) {
+  int curr1 = 1;
+  int curr2 = 1;
+  int next;
+
+  for (int i = 0; i < numRows; ++i) {
+    if (i == 0 || i == 1) {
+      COUT << "1 ";
+      continue;
+    }
+    next = curr1 + curr2;
+    curr1 = curr2;
+    curr2 = next;
+
+    COUT << next << " ";
+  }
+  COUT << ENDL;
+}
+
