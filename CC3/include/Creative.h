56a42cdaca7588a03b3aae0713d5672d66aeb9d1
Corey Wurl <cwurl@student06.cse.nd.edu>
2020-09-13 19:14:59 -0400

    CC 3 Update

--- CC3/include/Creative.h
+++ CC3/include/Creative.h
@@ -20,12 +20,15 @@ void grader() {
   char input;
   DoubleHash<std::string, int> table;
 
+  // Coninue to ask the user what they want to do until they want to stop
   while (true) {
     COUT << "What would you like to do?" << ENDL;
     COUT << "Enter a grade (e) / Change a grade (c) / Remove a grade (r) / Print grades (p) / Quit (q): ";
+    // Get user input on what they want to do
     CIN >> input;
     COUT << ENDL;
 
+    // Enter a grade
     if (input == 'e') {
       COUT << "Please enter a student name: ";
       CIN >> name;
@@ -35,15 +38,18 @@ void grader() {
       CIN >> grade;
       COUT << ENDL;
 
+      // Insert into the hash table the specified data
       table.insert( { name, grade } ); 
     
       COUT << "You have entered a grade of " << grade << " for " << name << ENDL;
     }
+    // Change a grade
     else if (input == 'c') {
       COUT << "Please enter a student name: ";
       CIN >> name;
       COUT << ENDL;
 
+      // Check if that entry exists in the hash table
       if (!table.contains(name)) {
         COUT << "A grade does not exist for this student" << ENDL;
         continue;
@@ -53,32 +59,42 @@ void grader() {
       CIN >> grade;
       COUT << ENDL;
 
+      // Store old value for printing purposes
       int temp = table[name];
+      // Change the value of grade
       table[name] = grade;
 
       COUT << "The grade for " << name << " has been changed from " << temp << " to " << grade << ENDL;
     }
+    // Remove a grade
     else if (input == 'r') {
       COUT << "Please enter a student name: ";
       CIN >> name;
       COUT << ENDL;
 
+      // Check if that entry exists in the hash table
       if (!table.contains(name)) {
         COUT << "A grade does not exist for this student" << ENDL;
         continue;
       }
 
+      // Store olf value for printing purposes
       int temp2 = table[name];
+      // Remove the value from the table
       table.remove(name);
 
       COUT << "The grade of " << temp2 << " for " << name << " has been removed" << ENDL;      
     }
+    // Print the grade
     else if (input == 'p') {
+      // Print the hash table using the overloaded operator
       COUT << table << ENDL; 
     }
+    // Quit out of the function/while loop
     else if (input == 'q') {
       break;
     }
+    // Prints if the user does not enter one of the specified chars
     else {
       COUT << "Please enter a valid character" << ENDL;
     }
