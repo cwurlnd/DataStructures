548d22fd6873ca8df8f53e0f7490673f01eb8a8d
Corey Wurl <cwurl@student06.cse.nd.edu>
2020-09-26 12:39:45 -0400

    CC 4 Update

--- CC4/src/Prob1.cpp
+++ CC4/src/Prob1.cpp
@@ -9,12 +9,22 @@
 
 #include "../include/Pacific.h"
 
-int main() {
-  VECTOR<VECTOR<int>> matrix { { 1, 2, 2, 3, 5 }, 
-                               { 3, 2, 3, 4, 4 }, 
+void test1 () {
+  COUT << "------------TEST 1--------------" << ENDL;
+
+  VECTOR<VECTOR<int>> matrix { { 1, 3, 4, 3, 7 }, 
+                               { 2, 2, 3, 4, 4 }, 
                                { 2, 4, 5, 3, 1 },
-                               { 6, 7, 1, 4, 5 },
-                               { 5, 1, 1, 2, 4 } };
+                               { 6, 7, 5, 4, 5 },
+                               { 5, 1, 6, 2, 4 } };
+
+  for (int i = 0; i < (int)matrix.size(); i++) { 
+    for (int j = 0; j < (int)matrix[i].size(); j++) 
+      COUT << matrix[i][j] << " "; 
+    COUT << ENDL; 
+  } 
+
+  COUT << ENDL << ENDL;
 
   VECTOR<VECTOR<int>> res = pacificAtlantic(matrix);
 
@@ -23,6 +33,69 @@ int main() {
       COUT << res[i][j] << " "; 
     COUT << ENDL; 
   } 
+  COUT << ENDL;
+}
+
+void test2 () {
+  COUT << "------------TEST 2--------------" << ENDL;
+
+  VECTOR<VECTOR<int>> matrix { { 1, 2, 2, 3, 5 }, 
+                               { 3, 2, 3, 4, 4 }, 
+                               { 2, 4, 5, 3, 1 },
+                               { 6, 7, 1, 4, 5 },
+                               { 5, 1, 1, 2, 4 } };
+
+  for (int i = 0; i < (int)matrix.size(); i++) { 
+    for (int j = 0; j < (int)matrix[i].size(); j++) 
+      COUT << matrix[i][j] << " "; 
+    COUT << ENDL; 
+  } 
+
+  COUT << ENDL << ENDL;
+
+  VECTOR<VECTOR<int>> res = pacificAtlantic(matrix);
+
+  for (int i = 0; i < (int)res.size(); i++) { 
+    for (int j = 0; j < (int)res[i].size(); j++) 
+      COUT << res[i][j] << " "; 
+    COUT << ENDL; 
+  } 
+  COUT << ENDL;
+}
+
+void test3 () {
+  COUT << "------------TEST 3--------------" << ENDL;
+
+  VECTOR<VECTOR<int>> matrix { { 1, 2, 2, 3, 5 }, 
+                               { 4, 4, 1, 4, 1 }, 
+                               { 2, 1, 5, 2, 2 },
+                               { 6, 1, 6, 6, 6 },
+                               { 7, 1, 7, 5, 4 } };
+
+  for (int i = 0; i < (int)matrix.size(); i++) { 
+    for (int j = 0; j < (int)matrix[i].size(); j++) 
+      COUT << matrix[i][j] << " "; 
+    COUT << ENDL; 
+  } 
+
+  COUT << ENDL << ENDL;
+
+  VECTOR<VECTOR<int>> res = pacificAtlantic(matrix);
+
+  for (int i = 0; i < (int)res.size(); i++) { 
+    for (int j = 0; j < (int)res[i].size(); j++) 
+      COUT << res[i][j] << " "; 
+    COUT << ENDL; 
+  } 
+  COUT << ENDL;
+}
+
+int main() {
+  test1();
+
+  test2();
+
+  test3();
 
   return 0;
 }
--- CC4/src/Prob2.cpp
+++ CC4/src/Prob2.cpp
@@ -9,12 +9,58 @@
 
 #include "../include/Rain.h"
 
-int main() {
+void test1() {
+  COUT << "-------------TEST 1---------------" << ENDL;
+
   int arr[] = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
 
   int n = (int)sizeof(arr) / (int)sizeof(arr[0]);
 
-  COUT << maxWater(arr, n) << ENDL;
+  for (int i = 0; i < n; ++i) {
+    COUT << arr[i] << " ";
+  }
+  COUT << ENDL << ENDL;
+
+  COUT << "Water amount: " << maxWater(arr, n) << ENDL << ENDL;
+}
+
+void test2() {
+  COUT << "-------------TEST 2---------------" << ENDL;
+
+  int arr[] = { 3, 0, 2, 0, 4 };
+
+  int n = (int)sizeof(arr) / (int)sizeof(arr[0]);
+
+  for (int i = 0; i < n; ++i) {
+    COUT << arr[i] << " ";
+  }
+  COUT << ENDL << ENDL;
+
+  COUT << "Water amount: " << maxWater(arr, n) << ENDL << ENDL;
+}
+
+
+void test3() {
+  COUT << "-------------TEST 3---------------" << ENDL;
+
+  int arr[] = { 5, 0, 3, 1, 1, 2, 4, 0, 3, 2, 1, 3 };
+
+  int n = (int)sizeof(arr) / (int)sizeof(arr[0]);
+
+  for (int i = 0; i < n; ++i) {
+    COUT << arr[i] << " ";
+  }
+  COUT << ENDL << ENDL;
+
+  COUT << "Water amount: " << maxWater(arr, n) << ENDL << ENDL;
+}
+
+int main() {
+  test1();
+
+  test2();
+
+  test3();
 
   return 0;
 }
